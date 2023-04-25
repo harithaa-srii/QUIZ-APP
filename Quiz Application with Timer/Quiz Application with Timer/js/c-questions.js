9da32e2163140bf81a6b3d63b3595aa598c4e443
Harithaa Srii <76534114+harithaa-srii@users.noreply.github.com>
2023-04-25 11:54:56 +0530

    Add files via upload

---
+++ Quiz Application with Timer/Quiz Application with Timer/js/c-questions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "All keywords in C are in ____________",
+    answer: "LowerCase letters",
+    options: [
+      "CamelCase letters",
+      "UpperCase letters",
+      "None of the mentioned",
+      "LowerCase letters"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which is valid C expression?",
+    answer: "int my_num = 100000;",
+    options: [
+      "int my_num = 100,000;",
+      "int my_num = 100000;",
+      "int $my_num = 10000;",
+      "int my_num = 100,000;"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following cannot be a variable name in C?",
+    answer: "volatile",
+    options: [
+      "volatile",
+      "export",
+      "friend",
+      "true"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following typecasting is accepted by C language?",
+    answer: "Widening & Narrowing conversions",
+    options: [
+      "Narrowing conversions",
+      "Widening & Narrowing conversions",
+      "Widening conversions",
+      "None of the mentioned"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Functions can return enumeration constants in C?",
+    answer: "true",
+    options: [
+      "depends on the compiler",
+      "depends on the standard",
+      "false",
+      "true"
+    ]
+  },
+   {
+     numb: 6,
+     question: " Property which allows to produce different executable for different platforms in C is called?",
+     answer: "Conditional compilation",
+     options: [
+       "Conditional compilation",
+       "Recursive macros",
+       "File inclusion",
+       "Selective inclusion"
+     ]
+   },
+   {
+    numb: 7,
+    question: "The C-preprocessors are specified with _________ symbol.",
+    answer: "#",
+    options: [
+       "$",
+       "&",
+       "#",
+       ">",
+     ]
+   },
+   {
+    numb: 8,
+    question: "What is the sizeof(char) in a 32-bit C compiler?",
+    answer: "1 Byte",
+    options: [
+       "1 bit",
+       "2 bit",
+       "2 Byte",
+       "1 Byte",
+     ]
+   },
+   {
+    numb: 9,
+    question: "What is short int in C programming?",
+    answer: "Short is the qualifier and int is the basic data type",
+    options: [
+       "All the options below",
+       "Qualifier",
+       "The basic data type of C",
+       "Short is the qualifier and int is the basic data type",
+     ]
+   },
+   {
+    numb: 10,
+    question: "Which keyword is used to prevent any changes in the variable within a C program?",
+    answer: "const",
+    options: [
+       "immutable",
+       "volatile",
+       "mutable",
+       "const",
+     ]
+   },
+   
+];
---
+++ Quiz Application with Timer/Quiz Application with Timer/js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which is not a feature of OOP in general definitions?",
+    answer: "Duplicate/Redundant data",
+    options: [
+      "Efficient Code",
+      "Code reusability",
+      "Modularity",
+      "Duplicate/Redundant data"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which feature of OOP indicates code reusability?",
+    answer: "Inheritance",
+    options: [
+      "Abstraction",
+      "Polymorphism",
+      "Inheritance",
+      "Encapsulation"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The feature by which one object can interact with another object is _____________",
+    answer: "Message Passing",
+    options: [
+      "Data Binding",
+      "Data transfer",
+      "Message Passing",
+      "Message reading"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which among the following, for a pure OOP language, is true?",
+    answer: "The language must follow all the rules of OOP",
+    options: [
+      "The language should follow 3 or more features of OOP",
+      "The language must follow only 3 features of OOP",
+      "The language must follow all the rules of OOP",
+      "The language should follow at least 1 feature of OOP"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The copy constructors can be used to ________",
+    answer: "Copy an object so that it can be passed to a function",
+    options: [
+      "Copy an object for type casting",
+      "Copy an object so that it can be passed to a function",
+      "Copy an object so that it can be passed to a class",
+      "Copy an object so that it can be passed to another primitive type variable"
+    ]
+  },
+   {
+     numb: 6,
+     question: "What happens when an object is passed by reference?",
+     answer: "Destructor is not called",
+     options: [
+       "Destructor is not called",
+       "Destructor is called at end of function",
+       "Destructor is called when called explicitly",
+       "Destructor is called when function is out of scope"
+     ]
+   },
+   {
+    numb: 7,
+    question: "Which operator can be used to free the memory allocated for an object in C++?",
+    answer: "delete",
+    options: [
+       "Unallocate",
+       "Free()",
+       "Collect",
+       "delete",
+     ]
+   },
+   {
+    numb: 8,
+    question: "How many types of access specifiers are provided in OOP (C++)?",
+    answer: "3",
+    options: [
+       "4",
+       "2",
+       "3",
+       "1",
+     ]
+   },
+   {
+    numb: 9,
+    question: "Which among the following can show polymorphism?",
+    answer: "Overloading <<",
+    options: [
+       "Overloading +=",
+       "Overloading <<",
+       "Overloading &&",
+       "Overloading ||",
+     ]
+   },
+   {
+    numb: 10,
+    question: "In which access should a constructor be defined, so that object of the class can be created in any function?",
+    answer: "Public",
+    options: [
+       "Protected",
+       "Any access specifier will work",
+       "Private",
+       "Public",
+     ]
+   },
+   
+];
